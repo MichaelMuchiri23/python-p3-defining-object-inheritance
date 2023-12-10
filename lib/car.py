b7d37bc0fafc792d03acefaf2f121b616d8221bb
Michael Muchiri <muchirimichael2222@gmail.com>
2023-12-10 21:35:48 +0300

    Done

--- lib/car.py
+++ lib/car.py
@@ -1,4 +1,12 @@
 from vehicle import Vehicle
 
 class Car(Vehicle):
-    pass
+    def go(self):
+        return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+
+
+#Case example
+ferrari = Car(1, 5)
+print(ferrari.go())
+porsche = Vehicle(1, 5)
+print(porsche.go())
